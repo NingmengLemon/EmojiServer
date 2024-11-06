7e77791d34123adf780fb950e9504e7c4976dbe6
NingmengLemon <60469383+NingmengLemon@users.noreply.github.com>
2024-11-06 09:23:15 +0800

    feat: add launch param `--port`

--- src/emojiserver/__main__.py
+++ src/emojiserver/__main__.py
@@ -1,6 +1,15 @@
+import argparse
+
 from aiohttp import web
 
 from .server import app
 
 
-web.run_app(app, port=8080)
+parser = argparse.ArgumentParser(description="EmojiServer")
+
+parser.add_argument("--port", type=int, default=8080, help="set port")
+
+args = parser.parse_args()
+
+
+web.run_app(app, port=args.port)
